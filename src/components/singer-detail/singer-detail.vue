3956262df64865a8b9c0dbaf2a90c9aa2c800250
kalebshuo <444947389@qq.com>
2018-06-12 16:34:04 +0800

    歌单详情页数据抓取

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -5,35 +5,54 @@
 </template>
 
 <script type='text/ecmascript-6'>
-import { mapGetters } from 'vuex' // 取数据的语法糖
+import { mapGetters } from 'vuex'; // 取数据的语法糖
+import { getSingerDetail } from 'api/singer';
+import { ERR_OK } from 'api/config';
 
 export default {
   computed: {
-    ...mapGetters([  // 使用对象展开运算符将此对象混入到外部对象中
+    ...mapGetters([
+      // 使用对象展开运算符将此对象混入到外部对象中
       'singer'
     ])
   },
   created() {
-    console.log(this.singer)
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer');
+        return;
+      }
+      getSingerDetail(this.singer.id).then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+        }
+      })
+    }
   }
 };
 </script>
 
 <style scoped lang='stylus' rel='stylesheet/stylus'>
-@import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .singer-detail 
-    position fixed
-    z-index 100
-    top 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
+.singer-detail {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: $color-background;
+}
 
-  .slide-enter-active, .slide-leave-active
-    transition: all 0.3s
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.3s;
+}
 
-  .slide-enter, .slide-leave-to
-    transform: translate3d(100%, 0, 0)
+.slide-enter, .slide-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
 </style>
