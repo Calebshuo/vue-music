c420954767f11d31232e8313801b8b5317d777cf
kalebshuo <444947389@qq.com>
2018-06-13 16:37:07 +0800

    music-list页面开发完毕

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,16 +1,32 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script type='text/ecmascript-6'>
-import { mapGetters } from 'vuex'; // 取数据的语法糖
-import { getSingerDetail } from 'api/singer';
-import { ERR_OK } from 'api/config';
+import { mapGetters } from 'vuex' // 取数据的语法糖
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import { createSong, isValidMusic } from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
+  components: {
+    MusicList
+  },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       // 使用对象展开运算符将此对象混入到外部对象中
       'singer'
@@ -27,26 +43,26 @@ export default {
       }
       getSingerDetail(this.singer.id).then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
+          this.songs = this._normalizeSongs(res.data.list)
         }
       })
-    }
+    },
+    _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item  // 对象的解构赋值
+          if (isValidMusic(musicData)) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
   }
 };
 </script>
 
 <style scoped lang='stylus' rel='stylesheet/stylus'>
 @import '~common/stylus/variable';
-
-.singer-detail {
-  position: fixed;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: $color-background;
-}
 
 .slide-enter-active, .slide-leave-active {
   transition: all 0.3s;
